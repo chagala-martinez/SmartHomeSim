e41551a08eda94c0bda30e92435e7a08fd46be7c
Alberto Chagala <42424250+achagala@users.noreply.github.com>
2021-05-06 20:48:24 -0700

    Asset
    Asset

---
+++ SmartHomeSim/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public float timerHealth = 0f;
+    public float timerFood = 0f;
+    public float timerPlay = 0f;
+    public float timerClean = 0f;
+    private float maxTimeHealth = 5f; //5
+    private float maxTimeFood = 5f; //15
+    private float maxTimePlay = 5f; //30
+    private float maxTimeClean = 5f; //20
+    public int health = 100;
+    public int food = 100;
+    public int play = 100;
+    public int clean = 100;
+
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = new Player(health, food, play, clean);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        CheckStatus();
+        CheckTime();
+    }
+
+    private void CheckTime()
+    {
+        if((timerHealth += Time.deltaTime) >= maxTimeHealth)
+        {
+            player.ModifyHealth(false);
+            health = player.GetHealth();
+            timerHealth = 0f;
+        }
+
+        if((timerFood += Time.deltaTime) >= maxTimeFood)
+        {
+            player.ModifyFood(false);
+            food = player.GetFood();
+            timerFood = 0f;
+        }
+
+        if((timerPlay += Time.deltaTime) >= maxTimePlay)
+        {
+            player.ModifyPlay(false);
+            play = player.GetPlay();
+            timerPlay = 0f;
+        }
+
+        if((timerClean += Time.deltaTime) >= maxTimeClean)
+        {
+            player.ModifyClean(false);
+            clean = player.GetClean();
+            timerClean = 0f;
+        }
+    }
+
+    private void CheckStatus()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            player.ModifyHealth(true);
+            health = player.GetHealth();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            player.ModifyFood(true);
+            food = player.GetFood();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            player.ModifyPlay(true);
+            play = player.GetPlay();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            player.ModifyClean(true);
+            clean = player.GetClean();
+        }
+    }
+}
---
+++ SmartHomeSim/Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int health;
+    private int food;
+    private int play;
+    private int clean;
+
+    public Player(int health, int food, int play, int clean)
+    {
+        this.health = health;
+        this.food = food;
+        this.play = play;
+        this.clean = clean;
+    }
+
+    public int GetHealth()
+    {
+        return this.health;
+    }
+
+    public int GetFood()
+    {
+        return this.food;
+    }
+
+    public int GetPlay()
+    {
+        return this.play;
+    }
+
+    public int GetClean()
+    {
+        return this.clean;
+    }
+
+    public void SetHealth(int health)
+    {
+        this.health = health;
+    }
+
+    public void SetFood(int food)
+    {
+        this.food = food;
+    }
+
+    public void SetPlay(int play)
+    {
+        this.play = play;
+    }
+
+    public void SetClean(int clean)
+    {
+        this.clean = clean;
+    }
+
+    public void ModifyHealth(bool add)
+    {
+        if(health < 100 && add)
+        {
+            health += 5;
+        }
+        else if(health > 15 && !add)
+        {
+            health -= 20;
+        }
+        else if(health <= 15 && !add)
+            health = 0;
+    }
+
+    public void ModifyFood(bool add)
+    {
+        if(food < 100 && add)
+        {
+            food += 5;
+        }
+        else if(food > 20 && !add)
+        {
+            food -= 25;
+        }
+        else if(food <= 20 && !add)
+            food = 0;
+    }
+
+    public void ModifyPlay(bool add)
+    {
+        if(play < 100 && add)
+        {
+            play += 5;
+        }
+        else if(play > 20 && !add)
+        {
+            play -= 25;
+        }
+        else if(play <= 20 && !add)
+            play = 0;
+    }
+
+    public void ModifyClean(bool add)
+    {
+        if(clean < 100 && add)
+        {
+            clean += 5;
+        }
+        else if(clean > 15 && !add)
+        {
+            clean -= 20;
+        }
+        else if(clean <= 15 && !add)
+            clean = 0;
+    }
+}
