eef369a06cf14ba8222adcdf6fa3c6a2bac74900
Alberto Chagala <42424250+achagala@users.noreply.github.com>
2021-05-13 21:02:47 -0700

    UI
    Pause Menu & Stats Panel

---
+++ SmartHomeSim/Assets/Scripts/StatsBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsBar : MonoBehaviour
+{
+    public Image healthBar;
+    public Image foodBar;
+    public Image playBar;
+    public Image cleanBar;
+    private float health;
+    private float play;
+    private float clean;
+    private float food;
+
+    private float maxValue = 100f;
+    
+    GameControl player;
+    
+    void Awake()
+    {
+        player = FindObjectOfType<GameControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = player.health;
+        healthBar.fillAmount = health / maxValue;
+
+        play = player.play;
+        playBar.fillAmount = play / maxValue;
+
+        food = player.food;
+        foodBar.fillAmount = food / maxValue;
+
+        clean = player.clean;
+        cleanBar.fillAmount = clean / maxValue;
+
+    }
+}
