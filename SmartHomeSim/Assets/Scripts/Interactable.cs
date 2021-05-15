293e5aa09b27c94e07406421f5d3402b01503c49
Jorge Martínez Villanueva <42475823+JorgeAntonioMtz@users.noreply.github.com>
2021-05-14 20:02:45 -0700

    scripts para interacciones
    Interacciones simples para tv, baño y cama

---
+++ SmartHomeSim/Assets/Scripts/Interactable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInsideZone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnMouseDown() {         //OnMazda
+        Interact();
+        Debug.Log("Interaccion OnMouseDown");
+    }
+
+    //Is called when the collider other enters the trigger
+    //<param name ="other">The other Collider involved in this collision.</param>
+     void OnTriggerEnter(Collider other) {
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = true;
+        //Debug.Log("Inside Zone");
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = false;
+        //Debug.Log("Outside Zone");
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interaccion");
+    }
+}
